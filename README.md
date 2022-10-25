bc28caff32d1359e3e29a63f2064a5a8441ac33d
mdutras <dutra22ms@gmail.com>
2022-10-25 16:42:05 -0300

    Implementei a ordenação das matrizes por peso, e substituir o iterador i pelos elementos em index

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Trabalho_SO
 Trabalho referente à disciplina CC0026 - Sistemas Operacionais.
 
-**Branch:** Original
-Esse branch contém a implementação "crua" do algoritmo do banqueiro.
+**Branch:** v1
+Esse branch contém a versão 1 com atualizações da implementação do algoritmo do banqueiro.
+
+**Mudanças na implementação**
+- Ordenação das listas utilizando a soma dos valores e pesos
+- Não usa mais 
--- banqueiro.py
+++ banqueiro.py
@@ -1,34 +1,56 @@
-from numpy import array, zeros
+from numpy import array, zeros, arange
+
+def mergeSort(A, index, start, end):
+    if((end - start) > 1):
+        mid = start + int((end - start) / 2)
+        mergeSort(A, index, start, mid)
+        mergeSort(A, index, mid, end)
+        for i in range(start, mid):
+            if(A[i] > A[mid]):
+                A[i], A[mid] = A[mid], A[i]
+                index[i], index[mid] = index[mid], index[i]
+            for i in range(mid, end -1):
+                if(A[i] > A[i+1]):
+                    A[i], A[i+1] = A[i+1], A[i]
+                    index[i], index[i+1] = index[i+1], index[i]
+                else:
+                    break
+
+def sortMatrix(procNec, recDisp):
+    pesos = array([sum(recDisp) / (num if num else 1) for num in recDisp])
+    sumM = array([sum(A * pesos) for A in procNec])
+    index = arange(len(procNec))
+    mergeSort(sumM, index, 0, len(procNec))
+    return index
 
 def banqueiro(procMax, procAlloc, recDisp):
     assert procMax.shape == procAlloc.shape
     assert procAlloc.shape[1] == recDisp.shape[0]
     procNec = procMax - procAlloc
-    # print(procNec)
+    index = sortMatrix(procNec, recDisp)
     termino = zeros(len(procNec))
-    i = 0
     hold = 0
     steps = 0
     print(" p  i")
     while(0 in termino and hold < len(procNec)):
-        steps += 1
-        if(i == len(procNec)):
-            i = 0
-        if(termino[i]):
-            print(f"({steps}, {i}) V")
-            hold += 1
-            i += 1
-            continue
-        if( all(recDisp[j] >= procNec[i][j] for j in range(len(recDisp))) ):
-            print(f"({steps}, {i}) C")
-            termino[i] = 1
-            recDisp = recDisp + procAlloc[i]
-            hold = 0
-        else:
-            hold += 1
-            print(f"({steps}, {i}) P")
-        i += 1
-    if(hold == len(procNec)):
+        for i in index:
+            if(0 not in termino or hold >= len(procNec)): #!TODO: Tentar um jeito melhor de fazer isso
+                break
+            steps += 1
+            if(termino[i]):
+                hold += 1
+                print(f"({steps}, {i}) V {hold}")
+                i += 1
+                continue
+            if( all(recDisp[j] >= procNec[i][j] for j in range(len(recDisp))) ):
+                print(f"({steps}, {i}) C")
+                termino[i] = 1
+                recDisp = recDisp + procAlloc[i]
+                hold = 0
+            else:
+                hold += 1
+                print(f"({steps}, {i}) P {hold}")
+    if(hold >= len(procNec)):
         print("Fuleco faleceu ;-;")
     else:
         print(f"Levou {steps} passos para completar o algoritmo :)")
@@ -37,20 +59,20 @@ def banqueiro(procMax, procAlloc, recDisp):
 def main():
     print("Hi >:3")
     procMax = array([
-        [7,5,3],
-        [3,2,2],
-        [9,0,2],
-        [2,2,2],
-        [4,3,3]
+        [5,1,1,7],
+        [3,2,1,1],
+        [3,3,2,1],
+        [4,6,1,2],
+        [6,3,2,5]
     ])
     procAlloc = array([
-        [0,1,0],
-        [2,0,0],
-        [3,0,2],
-        [2,1,1],
-        [0,0,2]
+        [3,0,1,4],
+        [2,2,1,0],
+        [3,1,2,1],
+        [0,5,1,0],
+        [4,2,1,2]
     ])
-    recDisp = array([3,3,2])
+    recDisp = array([0,3,0,1])
     banqueiro(procMax, procAlloc, recDisp)
 
 if __name__ == "__main__":
