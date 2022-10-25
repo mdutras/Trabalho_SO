983a8606a2cb4074ac2aa5604660b17fad71b701
mdutras <dutra22ms@gmail.com>
2022-10-25 14:30:48 -0300

    Substitui as listas por np.array

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Trabalho_SO
 Trabalho referente à disciplina CC0026 - Sistemas Operacionais.
+
+**Branch:** Original
+Esse branch contém a implementação "crua" do algoritmo do banqueiro.
--- banqueiro.py
+++ banqueiro.py
@@ -1,36 +1,33 @@
+from numpy import array, zeros
+
 def banqueiro(procMax, procAlloc, recDisp):
-    assert len(procMax) == len(procAlloc)
-    for i in range(len(procMax)):
-        assert len(procMax[i]) == len(procAlloc[i]) and len(procAlloc[i]) == len(recDisp)
-    procNec = [[procMax[i][j] - procAlloc[i][j] for j in range(len(recDisp))] for i in range(len(procMax))]
-    print(procNec)
-    termino = [False for i in range(len(procNec))]
+    assert procMax.shape == procAlloc.shape
+    assert procAlloc.shape[1] == recDisp.shape[0]
+    procNec = procMax - procAlloc
+    # print(procNec)
+    termino = zeros(len(procNec))
     i = 0
     hold = 0
     steps = 0
-    while(False in termino and hold < len(procNec)):
+    print(" p  i")
+    while(0 in termino and hold < len(procNec)):
+        steps += 1
         if(i == len(procNec)):
             i = 0
         if(termino[i]):
             print(f"({steps}, {i}) V")
-            i += 1
             hold += 1
-            steps += 1
+            i += 1
             continue
-        enough = True
-        for j in range(len(recDisp)):
-            if(recDisp[j] < procNec[i][j]):
-                enough = False
-                hold += 1
-        if(enough):
+        if( all(recDisp[j] >= procNec[i][j] for j in range(len(recDisp))) ):
             print(f"({steps}, {i}) C")
-            termino[i] = True
-            recDisp = [recDisp[j] + procAlloc[i][j] for j in range(len(recDisp))]
+            termino[i] = 1
+            recDisp = recDisp + procAlloc[i]
             hold = 0
         else:
+            hold += 1
             print(f"({steps}, {i}) P")
         i += 1
-        steps += 1
     if(hold == len(procNec)):
         print("Fuleco faleceu ;-;")
     else:
@@ -39,21 +36,21 @@ def banqueiro(procMax, procAlloc, recDisp):
 
 def main():
     print("Hi >:3")
-    procMax = [
+    procMax = array([
         [7,5,3],
         [3,2,2],
         [9,0,2],
         [2,2,2],
         [4,3,3]
-    ]
-    procAlloc = [
+    ])
+    procAlloc = array([
         [0,1,0],
         [2,0,0],
         [3,0,2],
         [2,1,1],
         [0,0,2]
-    ]
-    recDisp = [3,3,2]
+    ])
+    recDisp = array([3,3,2])
     banqueiro(procMax, procAlloc, recDisp)
 
 if __name__ == "__main__":
