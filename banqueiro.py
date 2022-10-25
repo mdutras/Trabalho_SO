220657c7bd3037c57d5e1906987ccea84b8df791
mdutras <dutra22ms@gmail.com>
2022-10-25 12:50:57 -0300

    Terminei o código (acho)

--- banqueiro.py
+++ banqueiro.py
@@ -3,7 +3,8 @@ def banqueiro(procMax, procAlloc, recDisp):
     for i in range(len(procMax)):
         assert len(procMax[i]) == len(procAlloc[i]) and len(procAlloc[i]) == len(recDisp)
     procNec = [[procMax[i][j] - procAlloc[i][j] for j in range(len(recDisp))] for i in range(len(procMax))]
-    termino = [False for i in range(procNec)]
+    print(procNec)
+    termino = [False for i in range(len(procNec))]
     i = 0
     hold = 0
     steps = 0
@@ -11,6 +12,7 @@ def banqueiro(procMax, procAlloc, recDisp):
         if(i == len(procNec)):
             i = 0
         if(termino[i]):
+            print(f"({steps}, {i}) V")
             i += 1
             hold += 1
             steps += 1
@@ -21,9 +23,12 @@ def banqueiro(procMax, procAlloc, recDisp):
                 enough = False
                 hold += 1
         if(enough):
+            print(f"({steps}, {i}) C")
             termino[i] = True
-            recDisp = [recDisp[j] + procAlloc[i][j] for j in range(recDisp)]
+            recDisp = [recDisp[j] + procAlloc[i][j] for j in range(len(recDisp))]
             hold = 0
+        else:
+            print(f"({steps}, {i}) P")
         i += 1
         steps += 1
     if(hold == len(procNec)):
@@ -34,6 +39,22 @@ def banqueiro(procMax, procAlloc, recDisp):
 
 def main():
     print("Hi >:3")
+    procMax = [
+        [7,5,3],
+        [3,2,2],
+        [9,0,2],
+        [2,2,2],
+        [4,3,3]
+    ]
+    procAlloc = [
+        [0,1,0],
+        [2,0,0],
+        [3,0,2],
+        [2,1,1],
+        [0,0,2]
+    ]
+    recDisp = [3,3,2]
+    banqueiro(procMax, procAlloc, recDisp)
 
 if __name__ == "__main__":
     main()
